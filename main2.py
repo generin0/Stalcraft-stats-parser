a1dbb9f8655cdfb225e4fdbf442eda113bef1640
Generin0 <dimas1234ssa@gmail.com>
2025-03-22 09:15:21 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
-import string
 
 url = input("input name : ")
 
@@ -33,25 +32,27 @@ def main():
     soup = BeautifulSoup(response.content, 'html.parser')
     comps = []
     items = soup.find_all('div', class_='mud-alert mud-alert-text-normal mud-dense mud-elevation-0 info-field') # main page
-    keys = []
-    values = []
 
     for item in items:
         title_div = item.find('div', class_='d-flex flex-column gap-1') # stats
         if title_div:
             comps.append({"title": title_div.text.strip().splitlines()})
-        else:
-            print("Wrong nickname.")
-
-    for comp in comps:
-        comp = comp.get('title', 0)
-        for item in comp:
-            if ':' in item:
-                key, value = item.split(':', 1)
-                keys.append(key)
-                values.append(value)
-
-    print_table(keys, values)
-
+    
+    if comps and not None:
+        split(comps)
+    else:
+        print("Wrong nickname.")
+    
+    def split(comps):
+        keys = []
+        values = []
+        for comp in comps:
+            comp = comp.get('title', 0)
+            for item in comp:
+                if ':' in item:
+                    key, value = item.split(':', 1)
+                    keys.append(key)
+                    values.append(value)
+            print_table(keys, values)
 main()
 input()
